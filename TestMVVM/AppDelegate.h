a1ed8b35442919dc0af775450e7f1cad780868f7
AlbertWu0212 <wzbdroid@gmail.com>
2016-03-02 13:32:30 +0800

    Initial Commit

---
+++ TestMVVM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestMVVM
+//
+//  Created by mxchip on 16/3/2.
+//  Copyright © 2016年 mxchip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
